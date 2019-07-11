7052120bfde68b00a1fe6b01725af3fb57dc1fae
MikuBlog <786601672@qq.com>
2019-07-11 13:38:07 +0800

    配置svg处理器

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  chainWebpack: config => {
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({
+        symbolId: "icon-[name]",
+        include: ["./src/icons"]
+      });
+  }
+};
