51de8c4c196f1937f4eadf2c393885f0cefd22c6
MikuBlog <786601672@qq.com>
2019-07-15 11:17:29 +0800

    封装下载文件函数

---
+++ src/api/dom/download.js
@@ -0,0 +1,15 @@
+/**
+ * @description 下载文件
+ * @param {String} url 下载路径
+ */
+function download(url) {
+    const 
+		ele = document.createElement('a'),
+		event = new MouseEvent('click')
+	ele.href = url
+	ele.dispatchEvent(event)
+}
+
+export default {
+    url
+}
