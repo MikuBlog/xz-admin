0ef05c74f3f5b5169428c373977cefb0605583d7
mikublog <786601672@qq.com>
2019-08-07 12:08:39 +0800

    统一图标数字渐变速度

--- src/views/chart/chart.vue
+++ src/views/chart/chart.vue
@@ -15,7 +15,7 @@
                                     流量
                                 </div>
                                 <div class="number">
-                                    <countTo :startVal="0" :endVal="peopleNum" :duration="3000"/>
+                                    <countTo :startVal="0" :endVal="peopleNum" :duration="duration"/>
                                 </div>
                             </div>
                         </el-col>
@@ -36,7 +36,7 @@
                                     留言
                                 </div>
                                 <div class="number">
-                                  <countTo :startVal="0" :endVal="messageNum" :duration="3000"/>
+                                  <countTo :startVal="0" :endVal="messageNum" :duration="duration"/>
                                 </div>
                             </div>
                         </el-col>
@@ -57,7 +57,7 @@
                                 销售额
                                 </div>
                                 <div class="number">
-                                  <countTo :startVal="0" :endVal="money" :duration="5000"/>
+                                  <countTo :startVal="0" :endVal="money" :duration="duration"/>
                                 </div>
                             </div>
                         </el-col>
@@ -78,7 +78,7 @@
                                 销售量
                                 </div>
                                 <div class="number">
-                                  <countTo :startVal="0" :endVal="sellNum" :duration="3000"/>
+                                  <countTo :startVal="0" :endVal="sellNum" :duration="duration"/>
                                 </div>
                             </div>
                         </el-col>
@@ -145,6 +145,7 @@ export default {
             sellNum: 573,
             messageNum: 79,
             speed: 40,
+            duration: 4000,
             lineData: {
                 columns: ['日期', '访问用户', '下单用户', '下单率'],
                 rows: [
