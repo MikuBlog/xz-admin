ce4867139304383737fc0bfd06017a5895d8a2bf
mikublog <786601672@qq.com>
2019-08-17 23:50:11 +0800

    减少代码体积

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ function resolve(dir) {
 
 module.exports = {
   chainWebpack: config => {
+    // 避免svg处理冲突
     config.module
       .rule('svg')
       .exclude.add(resolve('src/icons'))
@@ -20,6 +21,48 @@ module.exports = {
         symbolId: 'icon-[name]'
       })
       .end()
+    // 模块按需加载
+    config.plugins.delete('preload') // TODO: need test
+    config.plugins.delete('prefetch') // TODO: need test
+    // 代码分模块打包
+    config
+      .when(process.env.NODE_ENV !== 'development',
+        config => {
+          config
+            .plugin('ScriptExtHtmlWebpackPlugin')
+            .after('html')
+            .use('script-ext-html-webpack-plugin', [{
+              // `runtime` must same as runtimeChunk name. default is `runtime`
+              inline: /runtime\..*\.js$/
+            }])
+            .end()
+          config
+            .optimization.splitChunks({
+              chunks: 'all',
+              cacheGroups: {
+                libs: {
+                  name: 'chunk-libs',
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: 10,
+                  chunks: 'initial' // only package third parties that are initially dependent
+                },
+                elementUI: {
+                  name: 'chunk-elementUI', // split elementUI into a single package
+                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                },
+                commons: {
+                  name: 'chunk-commons',
+                  test: resolve('src/components'), // can customize your rules
+                  minChunks: 3, //  minimum common number
+                  priority: 5,
+                  reuseExistingChunk: true
+                }
+              }
+            })
+          config.optimization.runtimeChunk('single')
+        }
+      )
   },
   configureWebpack: {
     devtool: 'source-map'
