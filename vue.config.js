33bdcf6efc3b2aff843b83f9b39cb9123aeb9776
mikublog <786601672@qq.com>
2019-08-17 18:01:09 +0800

    解决svg处理问题

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,26 @@
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   chainWebpack: config => {
-    const svgRule = config.module.rule("svg");
-    svgRule.uses.clear();
-    svgRule
-      .use("svg-sprite-loader")
-      .loader("svg-sprite-loader")
+     // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
       .options({
-        symbolId: "icon-[name]",
-        include: ["./src/icons"],
-        exclude: ["./src/icons"],
-      });
+        symbolId: 'icon-[name]'
+      })
+      .end()
   },
   configureWebpack: {
     devtool: 'source-map'
