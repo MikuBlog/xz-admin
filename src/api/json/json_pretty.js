a352a0dcc3d2c076e33cb171956b5eb9d324f829
mikublog <786601672@qq.com>
2019-08-20 10:43:35 +0800

    强化代码健壮性

--- src/api/json/json_pretty.js
+++ src/api/json/json_pretty.js
@@ -6,11 +6,15 @@
  * @returns {JSON} 返回美化好的JSON 
  */
 function jsonPretty(json, tab = 4) {
+  try {
     if(typeof json == "object") {
-        return JSON.stringify(json, null, tab)
+      return JSON.stringify(json, null, tab)
     }else {
-        return JSON.stringify(JSON.parse(json), null, tab)
+      return JSON.stringify(JSON.parse(json), null, tab)
     }
+  }catch(e) {
+    return json
+  }
 }
 
 export default jsonPretty
