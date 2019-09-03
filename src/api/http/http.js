0e3e968ec099130c880ed4693cda87ebb540d394
mikublog <786601672@qq.com>
2019-09-03 16:15:47 +0800

    统一对url进行编码

--- src/api/http/http.js
+++ src/api/http/http.js
@@ -18,6 +18,7 @@
 				 * 如果是登陆请求，不发送token
 				 * 否则都带token过去
 				 */
+        config.url = encodeURI(config.url)
 				config.url.indexOf('login') != -1
 				? config.headers.Authorization = ""
 				: config.headers.Authorization = `Bearer ${storage.getMemoryPmt('token')}`
