41787151b9366d1a7124fc69298c07d2ca84c4cd
mikublog <786601672@qq.com>
2019-09-12 15:22:53 +0800

    更新获取url api

--- src/api/other/url_query.js
+++ src/api/other/url_query.js
@@ -1,20 +1,25 @@
-/**
- * @author xuanzai
- * @description 获取url后的参数
- * @returns {Object}
- */
-function urlQuery() {
-  let query = {}
-  if (window.location.search) {
-    let list = window.location.search.replace("?", "")
-    list.split("&").forEach(val => {
-      const
-        key = val.split("=")[0],
-        value = val.split("=")[1]
-      query[key] = value
-    })
-  }
-  return query
-}
+	/**
+	 * @author xuanzai
+	 * @description 获取url后的参数
+	 * @parmas { url } 传入的url，默认为空
+	 * @returns { Object }
+	 */
+	function urlQuery(url = "") {
+	  let 
+		query = {},
+		list
+	  if(url.split("?")[1]) {
+		list = url.split("?")[1]
+	  }else if (window.location.search) {
+	    list = window.location.search.replace("?", "")
+	  }
+	  list.split("&").forEach(val => {
+	    const
+	      key = val.split("=")[0],
+	      value = val.split("=")[1]
+	    query[key] = value
+	  })
+	  return query
+	}
 
-export default urlQuery
+	export default urlQuery
