15b498d43f37884a0453640aab1e7ccff3258c6f
mikublog <786601672@qq.com>
2019-10-21 08:52:38 +0800

    添加文件前缀路径

--- public/assets/js/baseUrl.js
+++ public/assets/js/baseUrl.js
@@ -1,2 +1,9 @@
+// 'https://aboot.missiono.cn'	
+// http://172.16.11.200:8087
+// http://172.16.14.33:8088
+// https://172.16.10.68:8087
 const 
-  baseUrl = 'http://172.16.11.200:8087'
+  // 请求Url
+  baseUrl = 'https://aboot.missiono.cn',
+  // 静态资源前缀地址
+  prefixUrl = 'https://aboot.missiono.cn'
