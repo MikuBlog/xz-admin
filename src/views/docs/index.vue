55f2018b4fb0a4104f2abaad853bfd83ac6773f8
mikublog <786601672@qq.com>
2019-11-12 17:23:14 +0800

    更新文档连接

--- src/views/docs/index.vue
+++ src/views/docs/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <iframe style="width: 100%; height: 100%" src="http://xzadmin-docs.xuanzai.top" frameborder="0"></iframe>
+  <iframe style="width: 100%; height: 100%" src="https://xzadmin-docs.xuanzai.top" frameborder="0"></iframe>
 </template>
