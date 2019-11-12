6412f7e7633e8b36337bee9e58b621b0e77694c1
mikublog <786601672@qq.com>
2019-11-12 16:37:49 +0800

    更新手册

--- public/assets/js/baseUrl.js
+++ public/assets/js/baseUrl.js
@@ -1,9 +1,10 @@
 // 'https://aboot.missiono.cn'	
 // http://172.16.11.200:8087
 // http://172.16.14.33:8088
-// https://172.16.10.68:8087
+// http://172.16.10.68:8087
+// http://172.16.15.74:8087
 const 
-  // 请求Url
+  // 接口统一请求前缀地址
   baseUrl = 'https://aboot.missiono.cn',
-  // 静态资源前缀地址
-  prefixUrl = 'https://aboot.missiono.cn'
+  // 静态资源请求前缀地址
+  prefixUrl = 'http://172.16.15.74:8087'
