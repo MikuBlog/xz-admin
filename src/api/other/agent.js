7958da810d5c354a30a7d017bd6d0f22de9626d7
mikublog <786601672@qq.com>
2019-11-29 19:13:43 +0800

    增加一些API

--- src/api/other/agent.js
+++ src/api/other/agent.js
@@ -3,6 +3,19 @@ function isIE() {
   return navigator.userAgent.toLowerCase().indexOf("trident") != -1
 }
 
+// 判断环境是否为移动端
+function isMobile() {
+  const mobileAgent = new Array("iphone", "ipod", "ipad", "android", "mobile", "blackberry", "webos", "incognito", "webmate", "bada", "nokia", "lg", "ucweb", "skyfire")
+  const browser = navigator.userAgent.toLowerCase()
+  for (var i = 0, len = mobileAgent.length; i < len; i++) {
+    if (browser.indexOf(mobileAgent[i]) != -1) {
+      return true
+    }
+  }
+  return false
+}
+
 export default {
-  isIE
+  isIE,
+  isMobile
 }
