1f9811732b878bafd91f32e2895facb8b749a481
mikublog <786601672@qq.com>
2019-12-02 08:33:49 +0800

    修复文档页面在ipad上无法正常显示问题

--- src/views/docs/index.vue
+++ src/views/docs/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <iframe style="width: 100%; height: 100%" src="https://xzadmin-docs.xuanzai.top" frameborder="0"></iframe>
+  <iframe style="width: 100%; height: calc(100% - 30px)" src="https://xzadmin-docs.xuanzai.top" frameborder="0"></iframe>
 </template>
+
+<style lang="scss" scoped>
+  iframe {
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+</style>
