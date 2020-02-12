1302a6ae7e4456223f2a55c250e6c163b2ebd871
mikublog <786601672@qq.com>
2020-02-12 21:24:04 +0800

    修复bug

--- src/views/redirect/index.vue
+++ src/views/redirect/index.vue
@@ -2,7 +2,10 @@
 export default {
   created() {
     const { query } = this.$route
-    this.$router.replace({ path: query.path })
+    this.$router.replace({ path: `${query.path}${Object.keys(query)
+		.filter(val => val !== 'path')
+		.map(val => `&${val}=${query[val]}`)
+		.join("")}` })
   },
   render(h) {
     return h()
