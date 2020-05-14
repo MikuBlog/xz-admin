e0a9eeb6fe930a6d270d8bd6d5c0156fdd9ecc21
mikublog <786601672@qq.com>
2020-05-14 16:40:13 +0800

    更新代码

--- public/assets/js/baseUrl.js
+++ public/assets/js/baseUrl.js
@@ -3,8 +3,10 @@
 // http://172.16.14.33:8088
 // http://172.16.10.68:8087
 // http://172.16.15.74:8087
+// http://localhost:8087
+// http://openlan.missiono.cn
 var
   // 接口统一请求前缀地址
-  baseUrl = 'http://192.168.1.103:8090',
+  baseUrl = 'http://192.168.1.133:8085',
   // 静态资源请求前缀地址
-  prefixUrl = 'http://192.168.1.103:8090'
+  prefixUrl = 'http://192.168.1.133:8085'
