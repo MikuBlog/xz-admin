aa80b09df74a281eafd2ce17f65de48bc08ae2d1
mikublog <786601672@qq.com>
2020-11-28 17:22:15 +0800

    更新代码

--- public_path.js
+++ public_path.js
@@ -1,4 +1,6 @@
+const path = require('path')
 module.exports = {
   environment: process.env.NODE_ENV === 'production' ? '/' : '/',
   editor: process.env.NODE_ENV === 'production' ? '/' : '/',
+  electron: path.resolve(__dirname, './dist')
 }
